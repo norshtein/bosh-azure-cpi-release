090849793cc5b62bac65bc5a669a7c94201b966d
norshtein <t-tosi@microsoft.com>
2018-04-26 08:28:40 +0000

    Update bash script to use Azure CLI 2.0

--- docs/advanced/application-gateway/cli/create-ag.sh
+++ docs/advanced/application-gateway/cli/create-ag.sh
@@ -39,6 +39,11 @@ if [ -z "$subnetName" ]; then
   subnetName="ApplicationGateway"
 fi
 
+read -p "Input the subnet address prefix [10.0.1.0/24]: " subnetAddressPrefix
+if [ -z "$subnetAddressPrefix" ]; then
+  subnetAddressPrefix="10.0.1.0/24"
+fi
+
 read -p "Input your public IP name[publicIP01]: " publicipName
 if [ -z "$publicipName" ]; then
   publicipName="publicIP01"
@@ -57,8 +62,8 @@ done
 
 while true; do
   read -p "Input the path of the certificate: " certPath
-  if [ -z "$certPath" ]; then
-    echo "Certificate can't be null"
+  if [ ! -f "$certPath" ]; then
+    echo "Given path is not a valid file"
   else
     break;
   fi
@@ -73,37 +78,41 @@ while true; do
   fi
 done
 
-# Remove it if the application gateway (AG) exists
-echo "Removing it if the application gateway exists"
-azure network application-gateway delete --resource-group $rgName --name $appgwName --quiet
+# Uncomment it if the application gateway (AG) exists
+# echo "Uncomment it if the application gateway exists"
+# az network application-gateway delete --resource-group $rgName --name $appgwName --quiet
 
 # Create public-ip
 echo "Creating public IP address for front end configuration"
-azure network public-ip create --resource-group $rgName --name $publicipName --location $location --allocation-method Dynamic
+az network public-ip create --resource-group $rgName --name $publicipName --location $location --allocation-method Dynamic
+
+# Create subnet for AG
+echo "Creating subnet for application gateway"
+az network vnet subnet create --resource-group $rgName --vnet-name $vnetName --name $subnetName --address-prefix $subnetAddressPrefix
 
 # Create AG
-azure network application-gateway create --resource-group $rgName --name $appgwName --location $location --vnet-name $vnetName --subnet-name $subnetName --http-settings-protocol http --http-settings-port 80 --http-settings-cookie-based-affinity Disabled --frontend-port 80 --public-ip-name $publicipName --frontend-ip-name $publicipName --http-listener-protocol http --routing-rule-type Basic --sku-name Standard_Small --sku-tier Standard --capacity $routerNumber
+az network application-gateway create --resource-group $rgName --name $appgwName --location $location --vnet-name $vnetName --subnet $subnetName --http-settings-protocol http --http-settings-port 80 --http-settings-cookie-based-affinity Disabled --frontend-port 80 --public-ip-address $publicipName --routing-rule-type Basic --sku Standard_Small --capacity $routerNumber
 
 # Create probe
 echo "Configuring a probe"
-hostName="login."$systemDomain
-azure network application-gateway probe create --resource-group $rgName --gateway-name $appgwName --name 'Probe01' --protocol Http --host-name $hostName --path '/' --interval 60 --timeout 60 --unhealthy-threshold 3 
+hostName="api."$systemDomain
+az network application-gateway probe create --resource-group $rgName --gateway-name $appgwName --name 'CustomProbe' --protocol Http --host $hostName --path '/' --interval 60 --timeout 60 --threshold 3
 
-# TODO
-# Need to set the custom probe for the http settings via Azure Portal or Azure Powershell
-# The next release of Azure CLI will supported setting the custom probe
+# Use custom probe in http settings
+az network application-gateway http-settings update --resource-group $rgName --gateway-name $appgwName --name 'appGatewayBackendHttpSettings' --probe 'CustomProbe'
 
 # Create HTTPS frontend-port
-azure network application-gateway frontend-port create --resource-group $rgName --gateway-name $appgwName --name frontendporthttps --port 443 --nowait
-azure network application-gateway frontend-port create --resource-group $rgName --gateway-name $appgwName --name frontendportlogs --port 4443 --nowait
+az network application-gateway frontend-port create --resource-group $rgName --gateway-name $appgwName --name frontendporthttps --port 443
+az network application-gateway frontend-port create --resource-group $rgName --gateway-name $appgwName --name frontendportlogs --port 4443
 
 # Create AG ssl-cert
-azure network application-gateway ssl-cert create --resource-group $rgName --gateway-name $appgwName --name 'cert01' --cert-file $certPath --cert-password $passwd
+az network application-gateway ssl-cert create --resource-group $rgName --gateway-name $appgwName --name 'cert01' --cert-file $certPath --cert-password $passwd
 
 # Create the listener and rule for frontend point 443
-azure network application-gateway http-listener create --resource-group $rgName --gateway-name $appgwName --name 'listener02' --frontend-ip-name $publicipName --frontend-port-name frontendporthttps --protocol https --ssl-cert 'cert01'
-azure network application-gateway rule create --resource-group $rgName --gateway-name $appgwName --name 'rule02' --type Basic --http-settings-name 'httpSettings01' --http-listener-name 'listener02' --address-pool-name 'pool01'
+az network application-gateway http-listener create --resource-group $rgName --gateway-name $appgwName --name 'appGatewayHttpsListener' --frontend-port frontendporthttps --ssl-cert 'cert01'
+az network application-gateway rule create --resource-group $rgName --gateway-name $appgwName --name 'HTTPSrule' --rule-type Basic --http-listener 'appGatewayHttpsListener' --address-pool 'appGatewayBackendPool'
 
 # Create the listener and rule for frontend point 4443
-azure network application-gateway http-listener create --resource-group $rgName --gateway-name $appgwName --name 'listener03' --frontend-ip-name $publicipName --frontend-port-name frontendportlogs --protocol https --ssl-cert 'cert01'
-azure network application-gateway rule create --resource-group $rgName --gateway-name $appgwName --name 'rule03' --type Basic --http-settings-name 'httpSettings01' --http-listener-name 'listener03' --address-pool-name 'pool01'
+az network application-gateway http-listener create --resource-group $rgName --gateway-name $appgwName --name 'appGatewayWebSocketListener' --frontend-port frontendportlogs --ssl-cert 'cert01'
+az network application-gateway rule create --resource-group $rgName --gateway-name $appgwName --name 'WebSocketsrule' --rule-type Basic --http-listener 'appGatewayWebSocketListener' --address-pool 'appGatewayBackendPool'
+
